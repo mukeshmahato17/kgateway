eb1e573cb7aaf658fcc01fd786e9aac7761bc7bd
mukeshmahato17 <mukeshmahato1089@gmail.com>
2025-05-23 23:15:00 +0545

    add yaml test
    Signed-off-by: mukeshmahato17 <mukeshmahato1089@gmail.com>

--- internal/kgateway/translator/gateway/gateway_translator_test.go
+++ internal/kgateway/translator/gateway/gateway_translator_test.go
@@ -506,6 +506,30 @@ var _ = DescribeTable("Basic GatewayTranslator Tests",
 			Name:      "example-gateway",
 		},
 	}),
+	Entry(
+		"http gateway with HTTP connection manager configuration",
+		translatorTestCase{
+			inputFile:  "http-connection-manager-config/http-gateway.yaml",
+			outputFile: "http-connection-manager-config/http-gateway-proxy.yaml",
+			gwNN: types.NamespacedName{
+				Namespace: "default",
+				Name:      "example-gateway",
+			},
+			assertReports: func(gwNN types.NamespacedName, reportsMap reports.ReportMap) {
+				route := &gwv1.HTTPRoute{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "example-route",
+						Namespace: "default",
+					},
+				}
+				routeStatus := reportsMap.BuildRouteStatus(context.TODO(), route, wellknown.GatewayControllerName)
+				Expect(routeStatus).NotTo(BeNil())
+				Expect(routeStatus.Parents).To(HaveLen(1))
+				resolvedRefs := meta.FindStatusCondition(routeStatus.Parents[0].Conditions, string(gwv1.RouteConditionResolvedRefs))
+				Expect(resolvedRefs).NotTo(BeNil())
+				Expect(resolvedRefs.Message).To(Equal("Service \"example-service\" not found"))
+			},
+		}),
 )
 
 var _ = DescribeTable("Route Delegation translator",
